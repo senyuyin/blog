5d87284fb69eb38028e699295e6b4c017d73594b
伍玉宝 <wuyubao@192.168.0.107>
2021-07-12 16:49:36 +0800

    react-create-app on Heroku

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import Home from "./pages/home/Home";
+import TopBar from "./components/topbar/TopBar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Context } from "./context/Context";
+import { useContext } from "react";
+
+function App() {
+
+  const {user} = useContext(Context);
+  
+  return (
+    <Router>
+    <TopBar/>
+    {/* <Home/> */}
+    {/* <Single/> */}
+    {/* <Write></Write> */}
+    {/* <Settings/> */}
+    {/* <Login/> */}
+    {/* <Register/> */}
+    <Switch>
+      <Route exact path='/'>
+        <Home/>
+      </Route>
+      <Route path='/register'>
+        {!user ? <Register/> : <Home/>}
+      </Route>
+      <Route path='/login'>
+        {!user? <Login/> : <Home/>}
+      </Route>
+      <Route path='/write'>
+        {user? <Write/> : <Home/>}
+      </Route>
+      <Route path='/settings'>
+        {user? <Settings/> : <Home/>}
+      </Route>
+      <Route path='/post/:postId'>
+        <Single/>
+      </Route>
+    </Switch>
+    </Router>
+  );
+}
+
+export default App;
